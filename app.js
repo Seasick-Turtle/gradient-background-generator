f2f10e797da7a6ec33eab3e6e5b0147fe3907a4d
Christopher Martin <martincdev@gmail.com>
2018-07-30 13:21:12 -0500

    refactor functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let rand = document.querySelector('.random');
 
-function setGradient() {
+const setGradient = () => {
   let css = document.querySelector('h3');
   let body = document.getElementById('gradient');
 
@@ -12,9 +12,9 @@ function setGradient() {
   // set the text value of h3 element to show css rule
   // ex: linear-gradient(to right, rgb(65, 175, 219), rgb(188, 236, 144));
   css.textContent = `${body.style.background};`;
-}
+};
 
-function generateColors() {
+const generateColors = () => {
   let min = Math.ceil(0);
   let max = Math.ceil(255);
   let color1Value = [];
@@ -34,16 +34,16 @@ function generateColors() {
   // combine values from loop to properly convert into usable hex code
   color1.value = `#${color1Value.join('')}`;
   color2.value = `#${color2Value.join('')}`;
-}
+};
 
-function randomize() {
+const randomize = () => {
   // call to generate random colors
   generateColors();
 
   // call setGradient to use new color values from generateColors()
   setGradient();
 
-}
+};
 
 // invoke to set the initial gradient
 setGradient();
